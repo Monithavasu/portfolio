e9b1b16c5105a44923d08f87a7196b6bc47e9f0d
Monitha Vasu <monithavasu2003@gmail.com>
2026-02-22 21:28:02 +0530

    Fix Hero import

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import About from "./Components/About.jsx"
-import Contact from "./Components/Contact.jsx"
-import Hero from "./components/Hero.jsx"
+import About from "./Components/About"
+import Contact from "./Components/Contact"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
-import Projects from "./Components/Projects.jsx"
-import Skills from "./Components/Skills.jsx"
+import Projects from "./Components/Projects"
+import Skills from "./Components/Skills"
 
 
 const App = () => {
